59b8aad827bfb78566e00dc5038c1d687fbbc99f
ramatamvan11221 <ramadityasuryap.3004@gmail.com>
2026-06-30 20:21:30 +0700

    Doa Malam

--- prayers.js
+++ prayers.js
@@ -1,99 +1,137 @@
-const prayers = [
-  {
-    title: "Doa Pagi",
-    paragraphs: [
-      "Terima kasih, Tuhan, untuk pagi ini dan hari yang baru.",
-      "Terima kasih pula untuk malam yang telah lewat, di mana kami boleh mengalami perlindungan-Mu dalam tidur kami.",
-      "Sertailah dan berkatilah hari ini: pekerjaan kami, keluarga kami, sahabat-sahabat kami dan semua kejadian serta orang akan kami jumpai hari ini.",
-      "Supaya pada malam hari nanti apabila kami menutup hari ini, kami patut melambungkan puji dan syukur kami kepada-Mu, Tuhan yang mahakasih."
-    ]
-  },
+const prayerGroups = {
 
-  {
-    title: "Pernyataan Iman",
-    paragraphs: [
-      "Allahku, aku percaya kepada-Mu dalam segala hal yang telah Kau ajarkan.",
-      "Sebab Engkau telah mengatakan dalam firman-Mu.",
-      "Dan firman-Mu adalah kebenaran."
-    ]
-  },
+    morning: [
 
-  {
-    title: "Pernyataan Pengharapan",
-    paragraphs: [
-      "Aku mengandalkan harapanku pada janji-Mu tentang kehidupan kekal.",
-      "Dan aku mohon belaskasih serta kekuatan dari-Mu hari ini.",
-      "Oh Allahku, aku mencintai Engkau dengan seluruh hatiku di atas segala-galanya.",
-      "Karena Engkau telah mengasihi aku dengan tiada batas.",
-      "Bantulah aku mengasihi orang lain seperti mengasihi diriku sendiri demi kasih-Mu",
-      "Biarlah aku makin lama makin mengasihi-Mu dalam hidupku kini, dan selama-lamanya."
-    ]
-  },
+    {
+      title: "Doa Pagi",
+      paragraphs: [
+        "Terima kasih, Tuhan, untuk pagi ini dan hari yang baru.",
+        "Terima kasih pula untuk malam yang telah lewat, di mana kami boleh mengalami perlindungan-Mu dalam tidur kami.",
+        "Sertailah dan berkatilah hari ini: pekerjaan kami, keluarga kami, sahabat-sahabat kami dan semua kejadian serta orang akan kami jumpai hari ini.",
+        "Supaya pada malam hari nanti apabila kami menutup hari ini, kami patut melambungkan puji dan syukur kami kepada-Mu, Tuhan yang mahakasih."
+      ]
+    },
+  
+    {
+      title: "Pernyataan Iman",
+      paragraphs: [
+        "Allahku, aku percaya kepada-Mu dalam segala hal yang telah Kau ajarkan.",
+        "Sebab Engkau telah mengatakan dalam firman-Mu.",
+        "Dan firman-Mu adalah kebenaran."
+      ]
+    },
+  
+    {
+      title: "Pernyataan Pengharapan",
+      paragraphs: [
+        "Aku mengandalkan harapanku pada janji-Mu tentang kehidupan kekal.",
+        "Dan aku mohon belaskasih serta kekuatan dari-Mu hari ini.",
+        "Oh Allahku, aku mencintai Engkau dengan seluruh hatiku di atas segala-galanya.",
+        "Karena Engkau telah mengasihi aku dengan tiada batas.",
+        "Bantulah aku mengasihi orang lain seperti mengasihi diriku sendiri demi kasih-Mu",
+        "Biarlah aku makin lama makin mengasihi-Mu dalam hidupku kini, dan selama-lamanya."
+      ]
+    },
+  
+    {
+      title: "Doa Kepada Hati Yesus Yang Mahakudus",
+      paragraphs: [
+        "Yesus yang mahamurah hati dan pengasih, aku mempersembahkan diriku hari ini dan tiap hari kepada hati-Mu yang mahakudus.",
+        "Hati Yesus yang mahakudus, kumohon kepada-Mu untuk membuat aku makin lama makin menyerupai Engkau.",
+        "Hati Yesus yang mahakudus, aku mengandalkan Engkau.",
+        "Hati Yesus yang mahakudus, aku mohon belas kasih-Mu.",
+        "Hati Yesus yang mahakudus, aku percaya akan kasih-Mu kepadaku.",
+        "Yesus yang penurut dan rendah hati, buatlah hatiku seperti hati-Mu."
+      ]
+    },
+  
+    {
+      title: "Doa Pasrah",
+      paragraphs: [
+        "Tuhan Yesus, Terangilah hati dan pikiran kami dengan Roh Kudus-Mu",
+        "Tuhan Yesus, ajarilah kami sabar di saat kami berdoa",
+        "Tuhan Yesus, ajarilah kami berdoa dengan baik",
+        "Tuhan Yesus, ajarilah kami lebih percaya kepada-Mu",
+        "Tuhan Yesus, ajarilah kami lebih pasrah kepada-Mu",
+        "Tuhan Yesus, kami mohon berkat-Mu",
+        "Tuhan Yesus, kami mohon rahmat-Mu",
+        "Tuhan Yesus, ajarilah kami untuk mau mengasihi sesama kami",
+        "Tuhan Yesus, ajarilah kami untuk mau mengampuni sesama kami",
+        "Tuhan Yesus, kami rindu kepada-Mu",
+        "Tuhan Yesus. kami cinta kepada-Mu",
+        "Tuhan Yesus, hadirlah, kami menyambut-Mu",
+        "Tuhan Yesus, kami pasrah kepada-Mu",
+        "<b>Ya Allah Yang Maha Kuasa, pada saat ini juga, jiwaku kuserahkan kepada-Mu",
+        "Karena Engkau yang mempunyai bumi ini dan aku ciptaan-Mu (3x)</b>",
+        "Amin."
+      ]
+    },
+  
+    {
+      title: "Doa Harapan",
+      paragraphs: [
+        "Allah, Bapa di surga, kasih setia-Mu kekal abadi.",
+        "Engkaulah tumpuan hidup dan harapanku.",
+        "Tanamkanlah dalam hatiku pengharapan yang teguh akan kasih dan kebaikan-Mu;",
+        "pengharapan yang menjiwai seluruh hidup Putra-Mu Yesus Kristus.",
+        "Berilah aku pengharapan yang kuat karena yakin bahwa Engkau selalu besertaku.",
+        "Semoga aku selalu menyandarkan diri kepada-Mu dalam suka dan duka.",
+        "Aku mohon pengharapan yang teguh supaya aku tidak mudah putus asa dalam penderitaan dan kekecewaan.",
+        "Bapa, semoga pengharapan yang kuat selalu menjiwai seluruh hidupku.",
+        "Dalam pengharapan itu aku akan membangun hidup dan masa depanku.",
+        "Dalam harapan itu pula aku percaya akan memperoleh hidup abadi bersama Engkau.",
+        "Ya Allah, teguhkanlah pengharapanku.",
+        "<div class='divider'></div>",
+        "<b>(Sampaikan Doa & Permohonanmu)</b>",
+        "<b>Bapa Kami...</b>",
+        "<b>Salam Maria...</b>",
+        "<b>Kemuliaan...</b>",
+        "<b>Terpujilah...</b>",
+        "Amin."
+      ]
+    },
+  
+    {
+      title: "Doa Mukjizat 3 Hari kepada Bunda Maria",
+      paragraphs: [
+        "Bunga terindah dari Gunung Karmel, Pohon Anggur yang berbuah lebat, Cahaya Kemuliaan Surga, Bunda Putra Allah yang terkasih, Perawan Tak Bernoda, tolonglah aku dalam kebutuhanku ini.",
+        "Ya Bintang Laut yang bersinar di tengah gelapnya hidupku, tuntunlah aku dan tunjukkanlah bahwa Engkau sungguh Ibuku.",
+        "Ya Santa Maria, Bunda Allah, Ratu Surga dan Bumi, dengan rendah hati dari lubuk hatiku yang terdalam, aku memohon pertolongan-Mu dalam pergumulanku ini.",
+        "<b>(Sebut permohonanmu di dalam hati)</b>",
+        "Tiada satu pun yang mampu menandingi kuasa doa dan perantaraan-Mu. Ya Maria yang dikandung tanpa noda dosa, doakanlah kami yang berlindung kepadamu.",
+        "Bunda yang penuh kasih, aku menyerahkan perkara ini sepenuhnya ke dalam tangan-Mu.",
+        "<b>(Ulangi 3 kali dengan penuh iman)</b>",
+        "Amin."
+      ]
+    }
 
-  {
-    title: "Doa Kepada Hati Yesus Yang Mahakudus",
-    paragraphs: [
-      "Yesus yang mahamurah hati dan pengasih, aku mempersembahkan diriku hari ini dan tiap hari kepada hati-Mu yang mahakudus.",
-      "Hati Yesus yang mahakudus, kumohon kepada-Mu untuk membuat aku makin lama makin menyerupai Engkau.",
-      "Hati Yesus yang mahakudus, aku mengandalkan Engkau.",
-      "Hati Yesus yang mahakudus, aku mohon belas kasih-Mu.",
-      "Hati Yesus yang mahakudus, aku percaya akan kasih-Mu kepadaku.",
-      "Yesus yang penurut dan rendah hati, buatlah hatiku seperti hati-Mu."
-    ]
-  },
+    ],
 
-  {
-    title: "Doa Pasrah",
-    paragraphs: [
-      "Tuhan Yesus, Terangilah hati dan pikiran kami dengan Roh Kudus-Mu",
-      "Tuhan Yesus, ajarilah kami sabar di saat kami berdoa",
-      "Tuhan Yesus, ajarilah kami berdoa dengan baik",
-      "Tuhan Yesus, ajarilah kami lebih percaya kepada-Mu",
-      "Tuhan Yesus, ajarilah kami lebih pasrah kepada-Mu",
-      "Tuhan Yesus, kami mohon berkat-Mu",
-      "Tuhan Yesus, kami mohon rahmat-Mu",
-      "Tuhan Yesus, ajarilah kami untuk mau mengasihi sesama kami",
-      "Tuhan Yesus, ajarilah kami untuk mau mengampuni sesama kami",
-      "Tuhan Yesus, kami rindu kepada-Mu",
-      "Tuhan Yesus. kami cinta kepada-Mu",
-      "Tuhan Yesus, hadirlah, kami menyambut-Mu",
-      "Tuhan Yesus, kami pasrah kepada-Mu",
-      "<b>Ya Allah Yang Maha Kuasa, pada saat ini juga, jiwaku kuserahkan kepada-Mu",
-      "Karena Engkau yang mempunyai bumi ini dan aku ciptaan-Mu (3x)</b>",
-      "Amin."
-    ]
-  },
+    night: [
 
-  {
-    title: "Doa Harapan",
-    paragraphs: [
-      "Allah, Bapa di surga, kasih setia-Mu kekal abadi.",
-      "Engkaulah tumpuan hidup dan harapanku.",
-      "Tanamkanlah dalam hatiku pengharapan yang teguh akan kasih dan kebaikan-Mu;",
-      "pengharapan yang menjiwai seluruh hidup Putra-Mu Yesus Kristus.",
-      "Berilah aku pengharapan yang kuat karena yakin bahwa Engkau selalu besertaku.",
-      "Semoga aku selalu menyandarkan diri kepada-Mu dalam suka dan duka.",
-      "Aku mohon pengharapan yang teguh supaya aku tidak mudah putus asa dalam penderitaan dan kekecewaan.",
-      "Bapa, semoga pengharapan yang kuat selalu menjiwai seluruh hidupku.",
-      "Dalam pengharapan itu aku akan membangun hidup dan masa depanku.",
-      "Dalam harapan itu pula aku percaya akan memperoleh hidup abadi bersama Engkau.",
-      "Ya Allah, teguhkanlah pengharapanku.",
-      "Amin."
-    ]
-  },
-
-  {
-    title: "Doa Mukjizat 3 Hari kepada Bunda Maria",
-    paragraphs: [
-      "Bunga terindah dari Gunung Karmel, Pohon Anggur yang berbuah lebat, Cahaya Kemuliaan Surga, Bunda Putra Allah yang terkasih, Perawan Tak Bernoda, tolonglah aku dalam kebutuhanku ini.",
-      "Ya Bintang Laut yang bersinar di tengah gelapnya hidupku, tuntunlah aku dan tunjukkanlah bahwa Engkau sungguh Ibuku.",
-      "Ya Santa Maria, Bunda Allah, Ratu Surga dan Bumi, dengan rendah hati dari lubuk hatiku yang terdalam, aku memohon pertolongan-Mu dalam pergumulanku ini.",
-      "<b>(Sebut permohonanmu di dalam hati)</b>",
-      "Tiada satu pun yang mampu menandingi kuasa doa dan perantaraan-Mu. Ya Maria yang dikandung tanpa noda dosa, doakanlah kami yang berlindung kepadamu.",
-      "Bunda yang penuh kasih, aku menyerahkan perkara ini sepenuhnya ke dalam tangan-Mu.",
-      "Amin.",
-      "<b>(Ulangi 3 kali dengan penuh iman)</b>"
-    ]
-  }
-];
-
+    {
+      title: "Doa Pada Akhir Hari",
+      paragraphs: [
+        "Tuhan, hari ini, yang boleh kuterima dari-Mu telah mendekati akhirnya.",
+        "Kalau kerenungkan kembali apa yang telah terjadi dan apa yang boleh kuterima dari-Mu, tak ada lain yang kupatut ucapkan melainkan mengakui kebesaran rencana-Mu.",
+        "Begitu banyak yang boleh kuterima dari-Mu, begitu banyak juga yang Kaubiarkan terjadi pada diriku, keluargaku, sahabat-sahabatku, dan rekan-rekanku.",
+        "Ada yang begitu mudah kuterima dengan suka cita, karena menyenangkan.",
+        "Semua itu merupakan berkat dan karunia dari-Mu.",
+        "Namun, Tuhan, ada juga hal-hal yang menyebabkan aku bertanya: \"Mengapa, Tuhan?\" dan sulit untuk kuterima, termasuk kekurangan dan dosa-dosaku.",
+        "Ampunilah aku, Tuhan, untuk apa adanya aku ini.",
+        "Dengan rendah hati kumohon: Buatlah aku menjadi orang makin hari makin berterimakasih kepada-Mu.",
+        "Buatlah aku menjadi orang yang berpasrah kepada kehendak-Mu dan rencana-Mu.",
+        "Buatlah hari esok dan diriku sesudah hari ini, makin baik.",
+        "Ku akan tidur untuk beristirahat.",
+        "Biarlah malaikat-malaikat-Mu melindungi aku dan keluargaku.",
+        "Bangunkan aku besok dan siramlah aku dengan berkat dan kasih karunia-Mu.",
+        "<div class='divider'></div>",
+        "<b>Bapa Kami...</b>",
+        "<b>Salam Maria...</b>",
+        "<b>Kemuliaan...</b>",
+        "<b>Terpujilah...</b>",
+        "Amin."
+      ]
+    }
+  ]
+};
--- script.js
+++ script.js
@@ -1,10 +1,60 @@
 const content = document.getElementById("content");
+const backButton = document.getElementById("backButton");
 
-function renderPrayers() {
+function showHome() {
+
+    backButton.hidden = true;
+
+    content.innerHTML = `
+        <section class="home">
+
+            <div class="home-card" onclick="showPrayers('morning')">
+
+                <div class="home-icon">
+                    <i data-lucide="sun"></i>
+                </div>
+
+                <h2>Doa Pagi</h2>
+
+                <div class="divider"></div>
+
+                <span class="open">
+                    Tap untuk membuka
+                </span>
+
+            </div>
+
+            <div class="home-card" onclick="showPrayers('night')">
+
+                <div class="home-icon">
+                    <i data-lucide="moon-star"></i>
+                </div>
+
+                <h2>Doa Malam</h2>
+
+                <div class="divider"></div>
+
+                <span class="open">
+                    Tap untuk membuka
+                </span>
+
+            </div>
+
+        </section>
+    `;
+
+    lucide.createIcons();
+}
+
+function showPrayers(group) {
+
+    backButton.hidden = false;
+
+    lucide.createIcons();
 
     content.innerHTML = "";
 
-    prayers.forEach((prayer) => {
+    prayerGroups[group].forEach(prayer => {
 
         const section = document.createElement("section");
 
@@ -22,6 +72,41 @@ function renderPrayers() {
 
     });
 
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
 }
 
-renderPrayers();
+backButton.addEventListener("click", () => {
+
+    showHome();
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
+
+showHome();
+
+const menuToggle = document.getElementById("menuToggle");
+const floatingActions = document.getElementById("floatingActions");
+
+menuToggle.onclick = () => {
+
+    floatingActions.classList.toggle("show");
+
+    const open = floatingActions.classList.contains("show");
+
+    menuToggle.innerHTML = open
+        ? '<i data-lucide="x"></i>'
+        : '<i data-lucide="settings-2"></i>';
+
+    lucide.createIcons();
+
+};
+
+lucide.createIcons();
